b14fc6c0559f8b14df035fcb2f814045b2ac65e0
Matt Everett <matt.g.everett@gmail.com>
2019-11-24 21:57:16 +0000

    Tidy up.

--- wifi.c
+++ wifi.c
@@ -1,15 +1,4 @@
-/* OTA example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <string.h>
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/event_groups.h"
 
 #include "esp_system.h"
 #include "esp_wifi.h"
@@ -18,6 +7,7 @@
 #include "nvs_flash.h"
 
 #include "wifi.h"
+#include "freertos/event_groups.h"
 
 static const char *TAG = "WIFI";
 
